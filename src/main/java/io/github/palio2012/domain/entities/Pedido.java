738af3e6b34ceb2eab80682d06e46a12af6c6e62
Palio2012 <daniasrayan.com@gmail.com>
2022-11-10 09:26:02 -0300

    Copy of final commit in spring-boot-guide-2

---
+++ src/main/java/io/github/palio2012/domain/entities/Pedido.java
@@ -0,0 +1,77 @@
+package io.github.palio2012.domain.entities;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table (name = "pedido")
+public class Pedido {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn (name = "cliente_id")
+    private Cliente cliente;
+
+    @Column (name = "data_pedido")
+    private LocalDate dataPedido;
+
+    @Column (name = "total", precision = 20, scale = 2)
+    private BigDecimal total;
+
+    @OneToMany (mappedBy = "pedido")
+    private List <ItemPedido> itens;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public LocalDate getDataPedido() {
+        return dataPedido;
+    }
+
+    public void setDataPedido(LocalDate dataPedido) {
+        this.dataPedido = dataPedido;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    public List<ItemPedido> getItens() {
+        return itens;
+    }
+
+    public void setItens(List<ItemPedido> itens) {
+        this.itens = itens;
+    }
+
+    @Override
+    public String toString() {
+        return "Pedido{" +
+                "id=" + id +
+                ", dataPedido=" + dataPedido +
+                ", total=" + total +
+                '}';
+    }
+}
---
+++ src/main/java/io/github/palio2012/domain/entities/Produto.java
@@ -0,0 +1,44 @@
+package io.github.palio2012.domain.entities;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Table (name = "produto")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column (name = "id")
+    private Integer id;
+
+    @Column (name = "descricao")
+    private String descricao;
+
+    @Column (name = "preco_unitario")
+    private BigDecimal preco;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+}
