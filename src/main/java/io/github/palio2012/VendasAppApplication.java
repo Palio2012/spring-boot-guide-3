6223bf36092260d0fc7d218e2a1507342b8cba64
Palio2012 <daniasrayan.com@gmail.com>
2022-11-10 09:50:55 -0300

    RestController implemented and endpoint tests

--- src/main/java/io/github/palio2012/VendasAppApplication.java
+++ src/main/java/io/github/palio2012/VendasAppApplication.java
@@ -1,14 +1,12 @@
 package io.github.palio2012;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VendasAppApplication {
 
-    public static void main(String[]args){
+    public static void main(String[] args) {
         SpringApplication.run(VendasAppApplication.class, args);
     }
-
 }
