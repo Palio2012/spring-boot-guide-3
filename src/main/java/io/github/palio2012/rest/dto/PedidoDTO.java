54b642fee0bae5741c81d62ddca0c1ddfa87be7f
Palio2012 <daniasrayan.com@gmail.com>
2022-11-11 15:59:40 -0300

    DTO model implementation

---
+++ src/main/java/io/github/palio2012/rest/dto/PedidoDTO.java
@@ -0,0 +1,15 @@
+package io.github.palio2012.rest.dto;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+
+public class PedidoDTO {
+
+    private Integer cliente;
+    private BigDecimal total;
+    private List<ItemPedidoDTO> items;
+
+
+
+}
